2b4d06f703d0a0800e03174e38e003ffe2b8d786
Jonathan Cady <joncady@uw.edu>
2019-02-07 20:03:16 -0800

    Updates

---
+++ precache-manifest.0d7d87b2f6de5461a280c8febdafaa3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f26ee912b64c5cb8d1c",
+    "url": "/draw-with-me/static/js/runtime~main.2f26ee91.js"
+  },
+  {
+    "revision": "9b4100cb9963a9d3e2aa",
+    "url": "/draw-with-me/static/js/main.9b4100cb.chunk.js"
+  },
+  {
+    "revision": "537a5cdadb7272fb0d24",
+    "url": "/draw-with-me/static/js/1.537a5cda.chunk.js"
+  },
+  {
+    "revision": "9b4100cb9963a9d3e2aa",
+    "url": "/draw-with-me/static/css/main.6c0e163e.chunk.css"
+  },
+  {
+    "revision": "537a5cdadb7272fb0d24",
+    "url": "/draw-with-me/static/css/1.918d881f.chunk.css"
+  },
+  {
+    "revision": "449782aac29ac46e16bfc774ef2c7df5",
+    "url": "/draw-with-me/index.html"
+  }
+];
